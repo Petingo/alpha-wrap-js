e1f1602d00c63778acb3c8bfcf2dbe0caabdda64
petingo <eyanghongbin0315@gmail.com>
2024-02-03 15:22:22 +0100

    Feat: done testing in C++

---
+++ src/serializer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petingo on 2024/2/3.
+//
+
+#include "serializer.h"
---
+++ src/serializer.h
@@ -0,0 +1,16 @@
+//
+// Created by Petingo on 2024/2/3.
+//
+
+#ifndef ALPHA_WRAP_JS_SERIALIZER_H
+#define ALPHA_WRAP_JS_SERIALIZER_H
+
+
+
+class serializer {
+
+};
+
+
+
+#endif //ALPHA_WRAP_JS_SERIALIZER_H
