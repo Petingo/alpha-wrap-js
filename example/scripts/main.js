049b39efebef64da90f235ff349d1340184af280
petingo <eyanghongbin0315@gmail.com>
2026-02-22 23:10:06 +0100

    Improve UI/UX

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -15,7 +15,7 @@ const windowSize = {
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
